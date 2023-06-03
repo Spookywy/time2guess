337c95d33cb4b13acead73813b7ff4e16c8f5aa8
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2023-06-03 19:35:36 +0200

    :lipstick: improve overall ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import NavigationBar, { Pages } from "@/components/navigationBar";
-import { faGamepad } from "@fortawesome/free-solid-svg-icons";
+import { faGamepad, faStopwatch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { useState } from "react";
@@ -17,12 +17,18 @@ export default function Home() {
       <h1 className="text-jet text-center text-6xl font-extrabold">
         Time 2 Guess
       </h1>
-      <Link
-        href="create-teams"
-        className="text-light-orange bg-jet rounded-xl p-5 m-5 text-xl flex items-center hover:bg-light-orange hover:text-jet border-4 border-jet font-bold"
-      >
-        Jouer <FontAwesomeIcon icon={faGamepad} className="text-3xl ml-2" />
-      </Link>
+      <div className="flex flex-grow flex-col items-center justify-end mb-12 md:mb-0 md:justify-center">
+        <FontAwesomeIcon
+          icon={faStopwatch}
+          className="text-dark-orange text-7xl"
+        />
+        <Link
+          href="create-teams"
+          className="text-light-orange bg-jet rounded-xl p-5 mt-5 text-xl flex items-center hover:bg-light-orange hover:text-jet border-4 border-jet font-bold"
+        >
+          Jouer <FontAwesomeIcon icon={faGamepad} className="text-3xl ml-2" />
+        </Link>
+      </div>
       <div className="mt-auto mb-5">
         <NavigationBar currentPage={currentPage} changePage={changePage} />
       </div>
