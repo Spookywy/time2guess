a802f024e2b0cfcb8f6879ecd35e70ee7e6acdd3
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2023-07-01 16:43:29 +0200

    :sparkles: add random words selection

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,4 +1,13 @@
+import fs from "fs";
+import shuffleArray from "./utils";
+
+const NUMBER_OF_WORDS_TO_PICK = 40;
+
 export default function Game() {
+  const words = fs.readFileSync("src/assets/words.txt", "utf-8").split("\n");
+  const shuffledWords = shuffleArray(words);
+  const randomWords = shuffledWords.slice(0, NUMBER_OF_WORDS_TO_PICK);
+
   return (
     <div>
       <h1>Game</h1>
