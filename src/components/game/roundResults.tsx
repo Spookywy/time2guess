78719e9db39ac6c62d76592511df40eea91527f1
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2023-07-16 18:08:11 +0200

    :sparkles: announce the winnner

--- src/components/game/roundResults.tsx
+++ src/components/game/roundResults.tsx
@@ -1,5 +1,5 @@
 import { RoundNumber, TeamResult } from "@/types/common";
-import { faRankingStar } from "@fortawesome/free-solid-svg-icons";
+import { faRankingStar, faTrophy } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/navigation";
 import SecondaryButton from "../buttons/secondaryButton";
@@ -19,27 +19,44 @@ export default function RoundResults({
 }: RoundResultsProps) {
   const router = useRouter();
 
+  const team1TotalScore =
+    team1Score.round1 + team1Score.round2 + team1Score.round3;
+  const team2TotalScore =
+    team2Score.round1 + team2Score.round2 + team2Score.round3;
+
+  const winningTeam = team1TotalScore > team2TotalScore ? 1 : 2;
+
   return (
     <div className="m-4 flex h-[calc(100%-var(--header-height))] flex-col items-center text-center text-2xl text-jet">
-      <h1 className="text-6xl font-extrabold">Fin de la Manche {round}</h1>
+      <h1 className="text-6xl font-extrabold text-dark-orange">
+        Fin de la Manche {round}
+      </h1>
       <FontAwesomeIcon
         icon={faRankingStar}
-        className="mt-8 text-8xl text-dark-orange sm:mt-16"
+        className="mt-8 text-8xl sm:mt-16"
       />
       <div className="mt-8 flex text-3xl sm:mt-16">
-        <div className="mr-8">
-          <h2 className="font-bold">Équipe 1</h2>
+        <div className="mr-8 flex flex-col gap-3">
+          <h2 className="font-extrabold text-dark-orange">Équipe 1</h2>
           <p>{team1Score.round1}</p>
           <p>{team1Score.round2}</p>
           <p>{team1Score.round3}</p>
+          <p className="font-bold">{team1TotalScore}</p>
         </div>
-        <div>
-          <h2 className="font-bold">Équipe 2</h2>
+        <div className="flex flex-col gap-3">
+          <h2 className="font-extrabold text-dark-orange">Équipe 2</h2>
           <p>{team2Score.round1}</p>
           <p>{team2Score.round2}</p>
           <p>{team2Score.round3}</p>
+          <p className="font-bold">{team2TotalScore}</p>
         </div>
       </div>
+      {round === 3 && (
+        <div className="mt-10 text-3xl text-dark-orange">
+          <FontAwesomeIcon icon={faTrophy} className="text-4xl" />
+          <p className="font-extrabold">L&apos;équipe {winningTeam} gagne !</p>
+        </div>
+      )}
       <div className="mt-auto py-4 md:py-8">
         {round !== 3 ? (
           <SecondaryButton label="Manche suivante" onClick={changeRound} />
