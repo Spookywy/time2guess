d5107955dcb27eedc1b0d4914da369b2f19f46b9
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2025-03-22 15:13:49 +0100

    🐛 fix words file not found

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,6 +3,6 @@ export const DEFAULT_ROUND_DURATION = 45;
 export const DEFAULT_IS_TIME_PENALTY_FEATURE_ENABLED = false;
 export const TIME_PENALTY = 5;
 export const GITHUB_REPO_URL = "https://github.com/Spookywy/time2guess";
-export const APP_VERSION = "1.6.0";
+export const APP_VERSION = "1.6.1";
 export const MINIMUM_NUMBER_OF_TEAMS = 2;
 export const MAXIMUM_NUMBER_OF_TEAMS = 4;
