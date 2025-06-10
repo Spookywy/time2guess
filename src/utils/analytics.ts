850357c0eafebac4e97fd30e417ff1e4602403d1
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2025-06-10 23:51:46 +0200

    📈 prevent events from being sent in development mode

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -3,6 +3,11 @@ import { APP_VERSION } from "./constants";
 // https://developers.google.com/analytics/devguides/collection/ga4/events?hl=fr&client_type=gtag
 export function sendEvent(event: string, params: Record<string, string>) {
   if (typeof window === "undefined") return;
+  if (process.env.NODE_ENV === "development") {
+    console.log("📈 event caught:", event, params);
+    return;
+  }
+
   window.gtag("event", event, {
     ...params,
     app_version: APP_VERSION,
