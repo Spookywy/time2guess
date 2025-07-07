77ef580f2de4879738e041042c338bd132ec7b65
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2025-07-07 23:57:13 +0200

    💸 deactivate google ad sense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GOOGLE_ANALYTICS_ID, GOOGLE_TAG_MANAGER_ID } from "@/utils/constants";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
@@ -14,9 +15,9 @@ export const metadata: Metadata = {
   title: "Time2Guess",
   description:
     "Le super jeu de société dans lequel les joueurs doivent deviner et faire deviner des mots.",
-  other: {
-    "google-adsense-account": "ca-pub-7375677810787994",
-  },
+  // other: {
+  //   "google-adsense-account": "ca-pub-7375677810787994",
+  // },
 };
 
 export default function RootLayout({
@@ -26,8 +27,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
-      <GoogleTagManager gtmId="GTM-PKZPZBHQ" />
-      <GoogleAnalytics gaId="G-MFWGFR4M69" />
+      {/* <GoogleAdSense gpId={GOOGLE_PUBLISHER_ID} /> */}
+      <GoogleTagManager gtmId={GOOGLE_TAG_MANAGER_ID} />
+      <GoogleAnalytics gaId={GOOGLE_ANALYTICS_ID} />
       <body className={`${inter.className} h-full bg-light-orange`}>
         {children}
         <SpeedInsights />
