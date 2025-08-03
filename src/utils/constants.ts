19d3ccda19a73253a8e3af3d4c772924019d4bb9
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2025-08-03 15:48:36 +0200

    🍱 add new words

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ export const DEFAULT_NUMBER_OF_WORDS_TO_PICK = 40;
 export const DEFAULT_ROUND_DURATION = 45;
 export const DEFAULT_IS_TIME_PENALTY_FEATURE_ENABLED = false;
 export const TIME_PENALTY = 5;
-export const APP_VERSION = "1.7.0";
+export const APP_VERSION = "1.7.1";
 export const MINIMUM_NUMBER_OF_TEAMS = 2;
 export const MAXIMUM_NUMBER_OF_TEAMS = 4;
 
