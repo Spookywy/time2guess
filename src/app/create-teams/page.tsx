99aac0dd9189221f6d65be632de2d6c3ffc7be02
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2025-08-03 11:22:25 +0200

    ✨ rework the "Create teams" page

--- src/app/create-teams/page.tsx
+++ src/app/create-teams/page.tsx
@@ -1,22 +1,27 @@
 "use client";
 import PrimaryButton from "@/components/buttons/primaryButton";
 import SecondaryButton from "@/components/buttons/secondaryButton";
+import SettingsModal from "@/components/modals/settingsModal";
+import { sendEvent } from "@/utils/analytics";
 import {
   MAXIMUM_NUMBER_OF_TEAMS,
   MINIMUM_NUMBER_OF_TEAMS,
 } from "@/utils/constants";
-import { faSquareArrowUpRight } from "@fortawesome/free-solid-svg-icons";
+import { useGetSettingsThroughLocalStorage } from "@/utils/utils";
+import {
+  faGear,
+  faSquareArrowUpRight,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { sendEvent } from "@/utils/analytics";
-import { useGetSettingsThroughLocalStorage } from "@/utils/utils";
 
 export default function Page() {
   const { replace, push } = useRouter();
   const [numberOfTeams, setNumberOfTeams] = useState(2);
   const { nbWords, roundDuration, isTimePenaltyFeatureEnabled } =
     useGetSettingsThroughLocalStorage();
+  const [showSettingsModal, setShowSettingsModal] = useState(false);
 
   useEffect(() => {
     const hasVisitedHomepage = sessionStorage.getItem("homepageVisited");
@@ -43,15 +48,25 @@ export default function Page() {
     push(`/game?numberOfTeams=${numberOfTeams}`);
   }
 
+  function handleOpenSettingsModal() {
+    setShowSettingsModal(true);
+    sendEvent("settings_modal_viewed", {
+      page: "createTeams",
+    });
+  }
+
+  function handleCloseSettingsModal() {
+    setShowSettingsModal(false);
+  }
+
   return (
     <main className="flex h-full flex-col items-center px-1 pt-5">
-      <h1 className="text-center text-6xl font-extrabold text-jet">
-        Formez les équipes
-      </h1>
+      <h1 className="text-center text-6xl font-extrabold text-jet">Prêt ?</h1>
       <div className="mt-16 flex items-center gap-4">
         <button
-          className="rounded-full bg-jet w-12 h-12 text-3xl text-light-orange hover:bg-dark-orange pb-1"
+          className="rounded-full bg-jet w-12 h-12 text-3xl text-light-orange hover:bg-dark-orange pb-1 disabled:opacity-50"
           onClick={handleMinusClick}
+          disabled={numberOfTeams === MINIMUM_NUMBER_OF_TEAMS}
         >
           -
         </button>
@@ -59,17 +74,15 @@ export default function Page() {
           {numberOfTeams} équipes
         </span>
         <button
-          className="rounded-full bg-jet w-12 h-12 text-3xl text-light-orange hover:bg-dark-orange pb-1"
+          className="rounded-full bg-jet w-12 h-12 text-3xl text-light-orange hover:bg-dark-orange pb-1 disabled:opacity-50"
           onClick={handlePlusClick}
+          disabled={numberOfTeams === MAXIMUM_NUMBER_OF_TEAMS}
         >
           +
         </button>
       </div>
       <div className="mt-8 flex h-full flex-col items-center text-center text-xl">
-        <p className="font-semibold">
-          Besoin d&apos;aide pour former les équipes ?
-        </p>
-        <p className="mt-8">
+        <p>
           Utilisez notre super générateur d&apos;équipe aléatoire{" "}
           <a
             className="whitespace-nowrap font-semibold text-dark-orange"
@@ -79,6 +92,19 @@ export default function Page() {
             Random Plouf <FontAwesomeIcon icon={faSquareArrowUpRight} />
           </a>
         </p>
+        <p className="font-semibold mt-8">
+          Envie de personnaliser votre expérience ?
+        </p>
+        <div className="flex gap-2 mt-3">
+          <p>Ouvrir les</p>
+          <button
+            className="text-dark-orange font-extrabold flex items-center gap-2"
+            onClick={handleOpenSettingsModal}
+          >
+            <span>Paramètres</span>
+            <FontAwesomeIcon icon={faGear} />
+          </button>
+        </div>
         <div className="mb-5 mt-auto flex flex-col sm:flex-row">
           <div className="mb-5 sm:mb-0 sm:mr-5">
             <SecondaryButton label="Retour" onClick={() => push("/")} />
@@ -86,6 +112,9 @@ export default function Page() {
           <PrimaryButton label="Lancer la partie" onClick={startGame} />
         </div>
       </div>
+      {showSettingsModal && (
+        <SettingsModal onClose={handleCloseSettingsModal} />
+      )}
     </main>
   );
 }
